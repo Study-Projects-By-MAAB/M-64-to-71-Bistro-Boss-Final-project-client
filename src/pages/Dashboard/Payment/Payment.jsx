1e02411e14e54c22d99036180006c8cb78ab00b1
maab-fw <maabfw@proton.me>
2024-05-27 22:08:56 +0600

    module 70 payment class started, stripe package installed

---
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import SectionTitle from "../../../components/SectionTitle/SectionTitle"
+
+const Payment = () => {
+    return (
+        <div>
+            <SectionTitle heading="Payment" subHeading="Payment to eat"></SectionTitle>
+        </div>
+    )
+}
+
+export default Payment
